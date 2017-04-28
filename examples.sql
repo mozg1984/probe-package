d8403993382e744ec5128e3d93412ee7df39d4fe
Khisamutdinov Radik <mozg1984@gmail.com>
2017-04-28 16:41:11 +0400

    Added correction

--- examples.sql
+++ examples.sql
@@ -3,12 +3,12 @@ declare
   l_captured_memory number; -- bytes
 begin
   -- to get elapsed time
-  probe_pkg.start_timer;
+  probe.start_timer;
   /* first piece of code */
-  l_elapsed_time := probe_pkg.elapsed_time;
+  l_elapsed_time := probe.elapsed_time;
 
   -- to get captured memory
-  probe_pkg.start_mem_watcher;
+  probe.start_mem_watcher;
   /* first piece of code */
-  l_captured_memory := probe_pkg.captured_mem;
+  l_captured_memory := probe.captured_mem;
 end;
